874bdeb2a29731483684fcd564a337c2413bbe59
guilhermeo97 <55219644+guilhermeo97@users.noreply.github.com>
2023-04-05 21:59:03 -0300

    init
    ...

---
+++ spring-mvc-thymeleaf/src/test/java/com/t3/springmvcthymeleaf/SpringMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t3.springmvcthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
